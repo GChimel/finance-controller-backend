72b5b7241724b5cabf203f956311eb3e12652e04
GChimel <gustavochimel12@gmail.com>
2025-01-20 06:47:37 -0300

    feat: Creating user modules

---
+++ src/users/users.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.prismaService.user.create({ data: createUserDto });
+
+    return user;
+  }
+}
