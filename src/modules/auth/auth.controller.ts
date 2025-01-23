74262dcc523def6006501358903309bce282f3b3
GChimel <gustavochimel12@gmail.com>
2025-01-23 06:37:57 -0300

    feat: Auth guard and public routes

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { IsPublic } from 'src/shared/decorators/isPublic';
 import { AuthService } from './auth.service';
 import { SigninDto } from './dto/signin.dto';
 import { SignupDto } from './dto/signup.dto';
@@ -7,12 +8,14 @@ import { SignupDto } from './dto/signup.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @IsPublic()
   @Post('signin')
   signin(@Body() signinDto: SigninDto) {
     return this.authService.signin(signinDto);
   }
 
   @Post('signup')
+  @IsPublic()
   create(@Body() signupDto: SignupDto) {
     return this.authService.signup(signupDto);
   }
